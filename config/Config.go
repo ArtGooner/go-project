eb9094d57a41b6f849c643c9830c2f1d874e6d4b
ArtGooner <artstop92@gmail.com>
2020-02-11 22:52:11 +0300

    Add env config

---
+++ config/Config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"github.com/caarlos0/env"
+)
+
+type Config struct {
+	ConnectionString string `env:"CONNECTION_STRING" envDefault:"host=localhost port=5432 user=postgres password=213612458 dbname=therion sslmode=disable"`
+	HttpPort         int    `env:"HTTP_PORT" envDefault:"8888"`
+	GrpcPort         int    `env:"GRPC_PORT" envDefault:"50051"`
+	GrpcAddress      string `env:"GRPC_ADDRESS" envDefault:"localhost:50051"`
+}
+
+// New config from environment variables
+func New() (*Config, error) {
+	c := &Config{}
+	err := env.Parse(c)
+	return c, err
+}
