7a175d886d6810192a227602f824580cf6a6a2ca
ArtGooner <artstop92@gmail.com>
2020-02-12 23:49:14 +0300

    Change http port type in config

--- user/config/Config.go
+++ user/config/Config.go
@@ -6,7 +6,7 @@ import (
 
 type Config struct {
 	ConnectionString string `env:"CONNECTION_STRING" envDefault:"host=localhost port=5432 user=postgres password=213612458 dbname=therion sslmode=disable"`
-	HttpPort         int    `env:"HTTP_PORT" envDefault:"8888"`
+	HttpPort         string `env:"HTTP_PORT" envDefault:"8888"`
 	GrpcPort         string `env:"GRPC_PORT" envDefault:":50051"`
 	GrpcAddress      string `env:"GRPC_ADDRESS" envDefault:"localhost:50051"`
 }
