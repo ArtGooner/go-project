56d9af4fffce421f66518ea6837268bc26e24e1e
ArtGooner <artstop92@gmail.com>
2020-03-30 15:59:48 +0300

    change local config

--- user/config/Config.go
+++ user/config/Config.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Config struct {
-	ConnectionString string `env:"CONNECTION_STRING" envDefault:"host=localhost port=5432 user=postgres password=213612458 dbname=therion sslmode=disable"`
+	ConnectionString string `env:"CONNECTION_STRING" envDefault:"host=localhost port=5432 user=artsiom password=213612458 dbname=artsiom sslmode=disable"`
 	HttpPort         string `env:"HTTP_PORT" envDefault:"8888"`
 	GrpcPort         string `env:"GRPC_PORT" envDefault:":50051"`
 	GrpcAddress      string `env:"GRPC_ADDRESS" envDefault:"localhost:50051"`
